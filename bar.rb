31fc757ed453ab61cd21587b7c6dd85ce6d14027
Morgan Wildermuth <morgan.wildermuth@gmail.com>
2014-10-21 00:34:26 -0700

    testing how many of each unit sold

--- bar.rb
+++ bar.rb
@@ -1,19 +1,18 @@
-class WildSpiritBar
-  attr_reader :number_of_people_in_venue
+class Bar
+  attr_reader :number_of_people_in_venue, :units_sold
 
-  def initialize(capacity_percentages)
-    @capacity_percentages = capacity_percentages
-    # @types_of_scales = {:high, :medium, :low}
-    # @types_of_units = {:beer, :cocktails_and_liquor, :wine, :non_alcoholic}
+  def initialize(percent_full_of_capacity)
+    @percent_full_of_capacity = percent_full_of_capacity
+    @units_sold = {}
     @capacity_of_venue = 60
-    # @buying_habit_percentages = {beer: 70, cocktails_and_liquor: 20, wine: 3, non_alcoholic: 7}
     @number_of_people_in_venue = calculate_number_of_people_in_venue
+    calculate_units_sold
   end
 
   def calculate_number_of_people_in_venue
-    low_capacity = percentage(@capacity_percentages[:low], @capacity_of_venue)
-    medium_capacity = percentage(@capacity_percentages[:medium], @capacity_of_venue)
-    high_capacity = percentage(@capacity_percentages[:high], @capacity_of_venue)
+    low_capacity = percentage(@percent_full_of_capacity[:low], @capacity_of_venue)
+    medium_capacity = percentage(@percent_full_of_capacity[:medium], @capacity_of_venue)
+    high_capacity = percentage(@percent_full_of_capacity[:high], @capacity_of_venue)
     {low: low_capacity, medium: medium_capacity, high: high_capacity}
   end
 
@@ -21,17 +20,28 @@ class WildSpiritBar
     (percentage*starting_value) / 100
   end
 
-  # def calculate_units_sold
-  #   @types_of_scale do |scale|
-  #     @types_of_units do |units|
-  #       buying_habit_percentages[scale][unit]/@number_of_people_in_venue[scale]
-  #     end
-  #   end
-  #   # buying_habit_percentages[:high][:beer]
-  #   # buying_habit_percentages[:high][:cocktails_and_liquor]
-  #   # buying_habit_percentages[:high][:wine]
-  #   # buying_habit_percentages[:high][:non_alcoholic]
-  # end
+  def calculate_units_sold
+    types_of_scale = [:high, :medium, :low]
+    types_of_units = [:beer, :cocktails_and_liquor, :wine, :non_alcoholic]
+    buying_habit_percentages = {beer: 70, cocktails_and_liquor: 20, wine: 3, non_alcoholic: 7}
+    types_of_scale.each do |scale|
+      @units_sold[scale] = {}
+      types_of_units.each do |unit|
+        p 'unit'
+        p unit
+        p percentage(buying_habit_percentages[unit], @number_of_people_in_venue[scale])
+        p 'venue'
+        p scale
+        p @number_of_people_in_venue[scale]
+        @units_sold[scale][unit] = percentage(buying_habit_percentages[unit], @number_of_people_in_venue[scale])
+      end
+    end
+    p @units_sold
+    # buying_habit_percentages[:high][:beer]
+    # buying_habit_percentages[:high][:cocktails_and_liquor]
+    # buying_habit_percentages[:high][:wine]
+    # buying_habit_percentages[:high][:non_alcoholic]
+  end
 end
 
 
@@ -44,10 +54,12 @@ def assert(e)
 end
 
 puts "Test Low Capacity Number of People in Bar Return"
-assert(WildSpiritBar.new({low: 10, medium: 20, high: 33}).number_of_people_in_venue[:low] == 6)
+assert(Bar.new({low: 10, medium: 20, high: 33}).number_of_people_in_venue[:low] == 6)
 puts "Test Medium Capacity Number of People in Bar Return"
-assert(WildSpiritBar.new({low: 10, medium: 20, high: 33}).number_of_people_in_venue[:medium] == 12)
+assert(Bar.new({low: 10, medium: 20, high: 33}).number_of_people_in_venue[:medium] == 12)
 puts "Test High Capacity Number of People in Bar Return"
-assert(WildSpiritBar.new({low: 10, medium: 20, high: 33}).number_of_people_in_venue[:high] == 19)
+assert(Bar.new({low: 10, medium: 20, high: 33}).number_of_people_in_venue[:high] == 19)
+puts "Test Low Capacity Drinks Sold"
+assert(Bar.new({low: 10, medium: 20, high: 33}).units_sold[:high] == {beer: 13, cocktails_and_liquor: 3, wine: 0, non_alcoholic: 1})
 
 
